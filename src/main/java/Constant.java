f76abef5174ddfa2e9fc2281b60d9586ce32b798
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-15 18:19:12 +0300

    Initial commit

---
+++ src/main/java/Constant.java
@@ -0,0 +1,26 @@
+public class Constant {
+    static final String HomePageURL = " http://localhost:4200/";
+
+    static final String HOME_PAGE_TITLE = "Home";
+    static final String QUESTION_PAGE_TITLE = "Questions";
+    static final String ABOUT_PAGE_TITLE = "About";
+
+    static final String HOME_PAGE_LINKTEXT = "Home";
+    static final String QUESTION_PAGE_LINKTEXT = "Question";
+    static final String ABOUT_PAGE_LINKTEXT = "About";
+
+    static final String TEAM_NAME = "DevFiksiki";
+
+    static final String QUESTION_PAGE_URL = "http://localhost:4200/questions.html";
+    static final String FILE_SYSTEM_DROPDOWN_ID = "file-system";
+    static final String QUESTION_THEME_DROPDOWN_ID = "questions-theme";
+    static final String NEW_QUESTION_BUTTON_ID = "new-question";
+    static final String HEADER_CLASS_NAME = "header";
+    static final String TEAM_NAME_CLASS_NAME = "header__team-name";
+    static final String LOGO_CLASS_NAME = "header__logo";
+    static final String DELETE_MESSAGE_BUTTON_ID = "delete-message";
+    static final String POP_UP_ID = "popup";
+    static final String DELETE_QUESTION_CONFIRM_BUTTON_ID = "confirm-btn";
+    static final String DELETE_QUESTION_CANCEL_BUTTON_ID = "cancel-btn";
+    static final String POP_UP_CLOSE_BUTTON_ID = "closePU";
+}
