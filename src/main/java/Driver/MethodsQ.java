c9436c44e90319d689bea3fa7f7fe2b012e1aa0d
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-16 17:44:33 +0300

    Update

--- src/main/java/Driver/MethodsQ.java
+++ src/main/java/Driver/MethodsQ.java
@@ -1,4 +1,9 @@
 package Driver;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class MethodsQ {
+    public MethodsQ() {
+        PageFactory.initElements(DriverConfig.getDriver(), this);
+    }
 }
