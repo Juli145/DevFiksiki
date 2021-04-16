ca67f30fa0210d95ae665e407483154bde620547
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-16 19:07:08 +0300

    Update

--- src/main/java/Driver/MethodsQ.java
+++ src/main/java/Driver/MethodsQ.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 @Getter
 
@@ -15,6 +17,54 @@ public class MethodsQ {
     @FindBy(css = "#new-question")
     private WebElement addQuestionButton;
 
+    @FindBy(css = "#create-modal-new-question")
+    private WebElement createButtonNewQuestion;
+
+    public static WebElement waitForVisibility(WebElement element, int timeOfWait, int... timeOfTryOut) {
+        WebElement webElement = null;
+        int timeOfRevision = timeOfTryOut.length == 0
+                ? 100
+                : timeOfTryOut[0];
+        try {
+            webElement = new WebDriverWait(DriverConfig.getDriver(),
+                    timeOfWait,
+                    timeOfRevision
+            ).until(ExpectedConditions.visibilityOf(element));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return webElement;
+    }
+
+    @FindBy(css = "#writeQuestion")
+    private WebElement questionField;
+
+    @FindBy(css = "#selectTheme")
+    private WebElement themeSelector;
+
+    @FindBy(css = "#answerTrue")
+    private WebElement answerTrue;
+
+    @FindBy(css = "#answerFalse")
+    private WebElement answerFalse;
+
+    @FindBy(css = "#CSV")
+    private WebElement CSV_checkBox;
+
+    @FindBy(css = "#JSON")
+    private WebElement JSON_checkBox;
+
+    @FindBy(css = "#XML")
+    private WebElement XML_checkBox;
+
+    @FindBy(css = "#YAML")
+    private WebElement YAML_checkBox;
+
+    @FindBy(css = "#cancel-modal-new-question")
+    private WebElement cancelButtonNewQuestion;
+
+    @FindBy(css = "#close-modal-new-question")
+    private WebElement cross;
 
 
 
--- src/main/java/QuestionsPage/MWAddQuestion.java
+++ src/main/java/QuestionsPage/MWAddQuestion.java
@@ -1,11 +1,71 @@
 package QuestionsPage;
 
 import Driver.DriverConfig;
+import Driver.Methods;
+import Driver.MethodsQ;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
 
 public class MWAddQuestion {
+
+    MethodsQ methodsQ = new MethodsQ();
+    Methods methods = new Methods();
+
     @Before
     public void setUp() {
         DriverConfig.initialize();
+        methods.openPage("QuestionsPage");
+    }
+
+    @Test
+    public void test_check_MW_elements(){
+        methodsQ.getAddQuestionButton().click();
+        MethodsQ.waitForVisibility(methodsQ.getCreateButtonNewQuestion(), 5);
+        Assert.assertTrue(
+                "Question field is not visible",
+                methodsQ.getQuestionField().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Theme selector is not visible",
+                methodsQ.getThemeSelector().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Answer true is not visible",
+                methodsQ.getAnswerTrue().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Answer false is not visible",
+                methodsQ.getAnswerFalse().isDisplayed()
+        );
+        Assert.assertTrue(
+                "CSV check box is not visible",
+                methodsQ.getCSV_checkBox().isDisplayed()
+        );
+        Assert.assertTrue(
+                "JSON check box is not visible",
+                methodsQ.getJSON_checkBox().isDisplayed()
+        );
+        Assert.assertTrue(
+                "XML check box is not visible",
+                methodsQ.getXML_checkBox().isDisplayed()
+        );
+        Assert.assertTrue(
+                "YAML check box is not visible",
+                methodsQ.getYAML_checkBox().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Create button is not visible",
+                methodsQ.getCreateButtonNewQuestion().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Cancel button is not visible",
+                methodsQ.getCancelButtonNewQuestion().isDisplayed()
+        );
+        Assert.assertTrue(
+                "Cross is not visible",
+                methodsQ.getCross().isDisplayed()
+        );
     }
 }
