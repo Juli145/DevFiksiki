d93793aaf0cf63747cf91851849cd83b7a60038d
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-16 17:49:08 +0300

    Update

--- src/main/java/Driver/MethodsQ.java
+++ src/main/java/Driver/MethodsQ.java
@@ -1,6 +1,9 @@
 package Driver;
 
+import lombok.Getter;
 import org.openqa.selenium.support.PageFactory;
+
+@Getter
 
 public class MethodsQ {
     public MethodsQ() {
--- src/main/java/QuestionsPage/MWAddQuestion.java
+++ src/main/java/QuestionsPage/MWAddQuestion.java
@@ -1,4 +1,11 @@
 package QuestionsPage;
 
+import Driver.DriverConfig;
+import org.junit.Before;
+
 public class MWAddQuestion {
+    @Before
+    public void setUp() {
+        DriverConfig.initialize();
+    }
 }
