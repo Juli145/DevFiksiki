2a2b64d2c4d44daddf5a4727a9a9f8268d12e826
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-17 20:18:11 +0300

    Update

--- src/main/java/Driver/MethodsQ.java
+++ src/main/java/Driver/MethodsQ.java
@@ -84,6 +84,8 @@ public class MethodsQ {
     @FindBy(id = "cancel-delete-question")
     private WebElement cancelDeleteQuestion;
 
+    @FindBy(id = "closePU")
+    private WebElement crossDeleteQuestionMW;
 
     public void openMW() {
         getAddQuestionButton().click();
--- src/main/java/QuestionsPage/MWDeleteQuestion.java
+++ src/main/java/QuestionsPage/MWDeleteQuestion.java
@@ -3,6 +3,7 @@ package QuestionsPage;
 import Driver.DriverConfig;
 import Driver.Methods;
 import Driver.MethodsQ;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,7 @@ public class MWDeleteQuestion {
     @Test
     public void test_opening_MW_delete_question(){
         methodsQ.getDeleteQuestionButton().click();
+        MethodsQ.waitForVisibility(methodsQ.getConfirmDeleteQuestion(), 5);
         Assert.assertTrue(
                 "Question field is not visible",
                 methodsQ.getConfirmDeleteQuestion().isDisplayed()
@@ -45,6 +47,7 @@ public class MWDeleteQuestion {
                 methodsQ.getConfirmDeleteQuestion().isDisplayed()
         );
     }
+
     @Test
     public void test_check_MW_DQ_cancelButton(){
         methodsQ.getDeleteQuestionButton().click();
@@ -59,9 +62,26 @@ public class MWDeleteQuestion {
     public void test_check_closing_MW_DQ_byClicking_cancelButton(){
         methodsQ.getDeleteQuestionButton().click();
         MethodsQ.waitForVisibility(methodsQ.getConfirmDeleteQuestion(), 5);
-        Assert.assertTrue(
-                "Confirm delete button is not visible",
+        methodsQ.getCancelDeleteQuestion().click();
+        Assert.assertFalse(
+                "Modal window is not closed",
                 methodsQ.getCancelDeleteQuestion().isDisplayed()
         );
+    }
+
+    @Test
+    public void test_check_closing_MW_DQ_byClicking_cross(){
+        methodsQ.getDeleteQuestionButton().click();
+        MethodsQ.waitForVisibility(methodsQ.getConfirmDeleteQuestion(), 5);
+        methodsQ.getCrossDeleteQuestionMW().click();
+        Assert.assertFalse(
+                "Modal window is not closed",
+                methodsQ.getCancelDeleteQuestion().isDisplayed()
+        );
+    }
+
+    @After
+    public void finish() {
+        DriverConfig.quit();
     }
 }
